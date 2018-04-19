4107f1e808c07f72af4b27203f2a2f4933b84a71
XuYibo <xuyibo@anji-plus.com>
2018-04-19 09:01:12 +0800

    转移配置到java文件

--- src/main/java/com/yibo/web/config/FastJSONConfig.java
+++ src/main/java/com/yibo/web/config/FastJSONConfig.java
@@ -1,15 +1,31 @@
 package com.yibo.web.config;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class FastJSONConfig {
     @Bean
-    public FastJsonConfig fastJsonConfig() {
+    public FastJsonHttpMessageConverter fastJsonHttpMessageConverter() {
+        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
         FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
-        return fastJsonConfig;
+        fastJsonConfig.setSerializerFeatures(
+                SerializerFeature.PrettyFormat,
+                SerializerFeature.WriteDateUseDateFormat,
+                SerializerFeature.WriteNullStringAsEmpty);
+        fastJsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
+        List<MediaType> mediaTypeList = new ArrayList<>();
+        mediaTypeList.add(MediaType.APPLICATION_JSON);
+        mediaTypeList.add(MediaType.APPLICATION_JSON_UTF8);
+        mediaTypeList.add(MediaType.valueOf("text/html;charset=UTF-8"));
+        fastJsonHttpMessageConverter.setSupportedMediaTypes(mediaTypeList);
+        return fastJsonHttpMessageConverter;
     }
 }
---
+++ src/main/java/com/yibo/web/config/MybatisConfig.java
@@ -0,0 +1,25 @@
+package com.yibo.web.config;
+
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class MybatisConfig {
+    @Bean
+    public static MapperScannerConfigurer mapperScannerConfigurer() {
+        MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
+        mapperScannerConfigurer.setBasePackage("com.yibo.web.dao");
+        return mapperScannerConfigurer;
+    }
+
+    @Bean
+    public static DataSourceTransactionManager transactionManager(DataSource dataSource) {
+        DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
+        transactionManager.setDataSource(dataSource);
+        return transactionManager;
+    }
+}
