2d94113b511e96235543a150bd996f6182d8641b
Yibo <xuyibo@outlook.com>
2019-06-22 19:46:10 +0800

    upgrade

--- src/main/java/com/yibo/web/dao/EmployeeDao.java
+++ src/main/java/com/yibo/web/dao/EmployeeDao.java
@@ -2,6 +2,8 @@ package com.yibo.web.dao;
 
 import com.yibo.web.entity.Employee;
 
+import java.util.List;
+
 public interface EmployeeDao {
     int deleteByPrimaryKey(Integer id);
 
@@ -14,4 +16,6 @@ public interface EmployeeDao {
     int updateByPrimaryKeySelective(Employee record);
 
     int updateByPrimaryKey(Employee record);
+
+    List<Integer> getIdList();
 }
--- src/main/java/com/yibo/web/job/EmployeeJob.java
+++ src/main/java/com/yibo/web/job/EmployeeJob.java
@@ -1,24 +1,31 @@
 package com.yibo.web.job;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.yibo.web.dao.EmployeeDao;
 import com.yibo.web.entity.Employee;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Random;
 
 @Component
 @Slf4j
 public class EmployeeJob {
-    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeJob.class);
+    private static final Random RANDOM = new Random();
     @Resource
     private EmployeeDao employeeDao;
 
     public void getEmployeeById() {
-        Employee employee = employeeDao.selectByPrimaryKey(1);
-        log.info(JSON.toJSONString(employee));
+        List<Integer> idList = employeeDao.getIdList();
+        if (CollectionUtils.isEmpty(idList)) {
+            return;
+        }
+        int size = idList.size();
+        Employee employee = employeeDao.selectByPrimaryKey(idList.get(RANDOM.nextInt(size)));
+        log.info("Get a random employee:\n{}", JSON.toJSONString(employee, SerializerFeature.PrettyFormat));
     }
 }
